fe3f00e5ec92c88426ab26b9ba48b84418d0973c
kmpxm <89741488+kmpxm@users.noreply.github.com>
2021-11-25 13:21:25 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -4,8 +4,13 @@
   <body>
     <table border="1">
       <?php
-        for($i=0;$i<5;$i++){
+        for($i=0;$i<100;$i++){
           echo "<tr>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์<td>";
+          echo "<td>คอลัมน์<td>";
           echo "<td>คอลัมน์</td>";
           echo "</tr>";
         }
@@ -13,3 +18,5 @@
     </table>
   </body>
 </html>
+เขียนถึง
+Aa
