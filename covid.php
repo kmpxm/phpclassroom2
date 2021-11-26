5aafeb1278c6c159e6af3951c4ec227c02c60333
kmpxm <89741488+kmpxm@users.noreply.github.com>
2021-11-26 14:11:13 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -10,3 +10,10 @@ echo
 "</pre>"
 ;
 ?>
+<?php
+  $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
+  $data = json_decode($json);
+  echo "<pre>";
+  var_dump($data);
+  echo "</pre>";
+?>
