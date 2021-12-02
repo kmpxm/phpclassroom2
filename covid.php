bae7f9bca285a509758f9da54da23de97591311d
kmpxm <89741488+kmpxm@users.noreply.github.com>
2021-12-02 22:01:05 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -1,8 +1,18 @@
 <?php
-  $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
   $data = json_decode($json);
-  echo $data[0]->new_case;
-  foreach ( $data[0] as $key => $val ){
-    echo $key.":".$val."<br>";
+
+  echo "<table border='1'>";
+
+ foreach ( $data[0] as $key => $val ){
+    echo "<tr>";
+    echo "<td>";
+    echo $key; 
+    echo "</td>";
+    echo "<td>";
+    echo $val."<br>";
+    echo "</td>";
+    echo "</tr>";
   }
-?>
+
+  echo "</table>";
